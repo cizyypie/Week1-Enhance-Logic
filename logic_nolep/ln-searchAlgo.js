9c2b3f3ca3b4cbac3587125e3ffbd39f1fbf1b1b
Joanita Timbin Panggalo <joanitatimbinp@gmail.com>
2025-09-10 22:53:18 +0700

    binary search

--- logic_nolep/ln-searchAlgo.js
+++ logic_nolep/ln-searchAlgo.js
@@ -1,32 +1,32 @@
 const searchRotatedArray = (nums, target) => {
-  //code
-  for(let i=0; i<= nums.length-1; i++){
-    if(i> i+1){
-        
-    }
-  }
-
   let left = 0;
-  let right = arr.length - 1;
+  let right = nums.length - 1;
 
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target) {
-      return mid; // Mengembalikan indeks elemen yang ditemukan
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
+
+    if (nums[mid] == target) {
+      return mid;
     }
-  }
-  return -1;
+      if (nums[left] <= nums[mid]) {
+        if (nums[left] <= target && target <= nums[mid]) {
+          right = mid - 1;
+        } else left = mid + 1;
+      } else {
+        if (nums[mid] <= target && target <= nums[right]) {
+          left = mid + 1;
+        } else right = mid - 1;
+      }
+    }
+  
+  return -1
 };
 
 // Test Case
 console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 0)); // Output: 4
 console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 3)); // Output: -1
 console.log(searchRotatedArray([1], 0)); // Output: -1
-console.log(searchRotatedArray([6, 7, 0, 1, 2, 4, 5], 7)); // Output: 1
+console.log(searchRotatedArray([0, 1, 2, 4, 5, 6, 7], 7)); // Output: 1
 console.log(searchRotatedArray([4, 5, 6, 7, 8, 1, 2, 3], 6)); // Output: 2
 console.log(searchRotatedArray([3, 4, 5, 6, 7, 8, 1, 2], 8)); // Output: 5
 console.log(searchRotatedArray([5, 6, 7, 8, 1, 2, 3, 4], 1)); // Output: 4
