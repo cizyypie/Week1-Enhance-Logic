e2c3b54d486174b0299e825b38a83b5c3833d998
Joanita Timbin Panggalo <joanitatimbinp@gmail.com>
2025-09-11 20:46:52 +0700

    create ln-sorting

--- logic_nolep/ln-searchAlgo.js
+++ logic_nolep/ln-searchAlgo.js
@@ -8,25 +8,24 @@ const searchRotatedArray = (nums, target) => {
     if (nums[mid] == target) {
       return mid;
     }
-      if (nums[left] <= nums[mid]) {
-        if (nums[left] <= target && target <= nums[mid]) {
-          right = mid - 1;
-        } else left = mid + 1;
-      } else {
-        if (nums[mid] <= target && target <= nums[right]) {
-          left = mid + 1;
-        } else right = mid - 1;
-      }
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target <= nums[mid]) {
+        right = mid - 1;
+      } else left = mid + 1;
+    } else {
+      if (nums[mid] <= target && target <= nums[right]) {
+        left = mid + 1;
+      } else right = mid - 1;
     }
-  
-  return -1
+  }
+  return -1;
 };
 
 // Test Case
 console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 0)); // Output: 4
 console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 3)); // Output: -1
 console.log(searchRotatedArray([1], 0)); // Output: -1
-console.log(searchRotatedArray([0, 1, 2, 4, 5, 6, 7], 7)); // Output: 1
+console.log(searchRotatedArray([6, 7, 0, 1, 2, 4, 5], 7)); // Output: 1
 console.log(searchRotatedArray([4, 5, 6, 7, 8, 1, 2, 3], 6)); // Output: 2
 console.log(searchRotatedArray([3, 4, 5, 6, 7, 8, 1, 2], 8)); // Output: 5
 console.log(searchRotatedArray([5, 6, 7, 8, 1, 2, 3, 4], 1)); // Output: 4
---
+++ logic_nolep/ln-sorting.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+const groupAnagrams = function(strs) {
+  // Implementasi akan datang di sini
+};
+
+// Test Case 1, hitung jumlah array a-z
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])); 
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Test Case 2
+console.log(groupAnagrams([""])); 
+// Output: [[""]]
+
+// Test Case 3
+console.log(groupAnagrams(["a"])); 
+// Output: [["a"]]
+
+// Test Case 4, 
+console.log(groupAnagrams(["listen", "silent", "hello", "world"])); 
+// Output: [["listen","silent"],["hello"],["world"]]
+
+// Test Case 5
+console.log(groupAnagrams(["rat", "tar", "art", "car"])); 
+// Output: [["rat","tar","art"],["car"]]
+
+// Test Case 6
+console.log(groupAnagrams(["apple", "banana", "leapp", "grape", "orange"])); 
+// Output: [["apple","leapp"],["banana"],["grape"],["orange"]]
+
+// Test Case 7
+console.log(groupAnagrams(["abcd", "dcba", "xyz", "zyx", "wxyz"])); 
+// Output: [["abcd","dcba"],["xyz","zyx"],["wxyz"]]
